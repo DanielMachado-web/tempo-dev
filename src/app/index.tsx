576793ee821294e974e3ea5847e3a6107950eaf0
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-28 23:48:07 -0300

    ALTERAÇÃO

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,14 @@
-
-
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App(){
 
 
     return(
         <View> 
-            <h1>Hello, Tempo!</h1>
+            <Text>OLA, APP!</Text>
          </View>
 
     )
 
-}import React from 'react';
-import { View } from 'react-native';
+}
