f733cdc11c4ce859483405e265db75186f48cc42
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-29 00:17:42 -0300

    alteração

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,18 @@
 import { View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
 
     return (
-        <View>
+        <SafeAreaView>
             <Text>OLA, APP!</Text>
             <TouchableOpacity onPress={() => alert("clicado")}
                 style={{ backgroundColor: 'blue', padding: 10, marginTop: 30 , borderRadius: 5, alignItems: 'center', justifyContent: 'center' }}
                 >
                 <Text>clique aqui !</Text>
             </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     )
 
 }
