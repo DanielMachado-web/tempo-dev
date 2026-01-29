42228899b22cad4b183cead6bda6a7cdbbe921fb
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-28 23:45:02 -0300

    alteração no projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function App(){
+
+
+    return(
+        <View> 
+            <h1>Hello, Tempo!</h1>
+         </View>
+
+    )
+
+}import React from 'react';
+import { View } from 'react-native';
