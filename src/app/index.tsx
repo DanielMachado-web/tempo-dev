34f1745c7edec72627f84d2bbd26b1e12bd0bd60
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-29 00:10:32 -0300

    alteração inclusao style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,9 @@ export default function App() {
     return (
         <View>
             <Text>OLA, APP!</Text>
-            <TouchableOpacity onPress={() => alert("clicado")}>
+            <TouchableOpacity onPress={() => alert("clicado")}
+                style={{ backgroundColor: 'blue', padding: 10, marginTop: 10 }}
+                >
                 <Text>clique aqui !</Text>
             </TouchableOpacity>
         </View>
