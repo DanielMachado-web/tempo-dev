baaa5ca9cc923c0f78abf8cf465468a694c628ed
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-30 01:09:53 -0300

    alteração

--- src/styles/home.styles.ts
+++ src/styles/home.styles.ts
@@ -3,6 +3,13 @@ import { colors, spacing, typography} from './colors';
 
 export const homeStyles = StyleSheet.create({
 
+    safeArea: {
+        flex: 1,
+        backgroundColor: colors.background,
+        paddingHorizontal: spacing.md,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 
 });
 
