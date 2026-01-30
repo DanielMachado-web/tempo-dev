5729f47edba17be04a92f1f28a94cede8d3f2049
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-30 01:13:51 -0300

    alteração

--- src/styles/home.styles.ts
+++ src/styles/home.styles.ts
@@ -9,7 +9,31 @@ export const homeStyles = StyleSheet.create({
         paddingHorizontal: spacing.md,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+
+    container{
+        flex: 1,
+
+    },
+
+    header{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        paddingTop: spacing.md,
+        paddingBottom: spacing.lg,  
+        paddingHorizontal: spacing.md,
+
+    },
+
+    title: {
+        fontSize: typography.lg,
+        fontWeight: 'bold',
+        color: colors.textPrimary,
+        marginBottom: spacing.md,
+    },
+
 
 });
 
