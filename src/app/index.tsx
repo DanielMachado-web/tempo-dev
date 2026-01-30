1967c2d23be06d087a81a349c29b65ecb0641d51
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-29 23:42:43 -0300

    button inclusao

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,9 +9,7 @@ export default function App() {
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle="dark-content"/>
             <Text style={styles.title}>OLA, APP!</Text>
-            <TouchableOpacity onPress={() => alert("clicado")}
-                style={{ backgroundColor: 'blue', padding: 10, marginTop: 30 , borderRadius: 5, alignItems: 'center', justifyContent: 'center' }}
-                >
+            <TouchableOpacity onPress={() => alert("clicado")}style={styles.button}>   
                 <Text>clique aqui !</Text>
             </TouchableOpacity>
         </SafeAreaView>
@@ -34,5 +32,15 @@ const styles = StyleSheet.create({
         color: '#ffff',
         marginBottom: 20,
 
+    },
+
+    button : {
+        backgroundColor: 'blue',
+        padding: 10,
+        marginTop: 30,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
+
 });
