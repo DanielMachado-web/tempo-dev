ac96dab6084e3fa3afb2118de9763801d33e5cec
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-30 23:46:18 -0300

    alterçaão

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TextInput } from 'react-native';
+
+interface SearchBarProps {
+
+    onSearch: (cityNamery: string) => void;
+    loading: boolean;
+
+}
+
+
+
+export default function SearchBar({ onSearch, loading = false }: SearchBarProps) {
+    return (
+
+        <View>
+            <View>
+                <TextInput placeholder="Digite o nome da cidade" style={{ borderWidth: 1, borderColor: '#000', padding: 10, borderRadius: 5 }} />
+            </View>
+        </View>
+    );
+}
