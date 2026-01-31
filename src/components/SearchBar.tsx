444735a24d301adec1bd467e6b4d8ef9056c60f8
DanielMachado-web <danielmachadojhs@gmail.com>
2026-01-30 23:53:10 -0300

    alteração

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,11 @@ const [searchText, setSearchText] = useState('');
  
         <View>
             <View>
-                <TextInput placeholder="Digite o nome da cidade" style={{ borderWidth: 1, borderColor: '#000', padding: 10, borderRadius: 5 }} />
+                <TextInput 
+                placeholder="Digite o nome da cidade" 
+                value={searchText}
+                onChangeText={setSearchText}
+                style={{ borderWidth: 1, borderColor: '#000', padding: 10, borderRadius: 5 }} />
             </View>
         </View>
     );
