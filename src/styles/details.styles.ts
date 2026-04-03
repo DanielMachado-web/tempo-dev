6d908f773ebc92d4ef041c1248cede2c239771dd
DanielMachado-web <danielmachadojhs@gmail.com>
2026-04-03 18:08:36 -0300

    alteração no projeto

---
+++ src/styles/details.styles.ts
@@ -0,0 +1,19 @@
+import { Button, StyleSheet } from 'react-native';
+import { colors, spacing, typography } from './colors';
+
+export const detailsStyles = StyleSheet.create({
+
+    safeArea: {
+        flex: 1,
+        backgroundColor: colors.background,
+    }
+
+
+})
+
+
+
+
+
+    
+    
